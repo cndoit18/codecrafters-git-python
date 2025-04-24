74a38ab52d8b09bde42e6ebd238d93b5f6d80b7b
cndoit18 <cndoit18@outlook.com>
2025-04-24 13:01:19 +0800

    feat: add ls-tree command
    Change-Id: I3da445ba7de198383a39e8682497e35f6eb1aa76

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import os
 import zlib
 import click
 import hashlib
+import itertools
 
 
 @click.group()
@@ -47,9 +48,36 @@ def cat_file(p: str):
             raw = zlib.decompress(f.read())
             head, content = raw.split(b"\0", 1)
             _, size = head.split(b" ")
-            if len(content) != int(size):
-                raise RuntimeError(f"Invalid object {p}")
+            assert len(content) == int(size)
             print(content.decode("utf-8"), end="")
+
+
+@git.command(name="ls-tree")
+@click.argument("tree_ish", type=str)
+@click.option("--name-only", is_flag=True, help="list only filenames")
+def ls_tree(tree_ish: str, name_only: bool):
+    with open(f".git/objects/{tree_ish[0:2]}/{tree_ish[2:]}", "rb") as f:
+        raw = zlib.decompress(f.read())
+        head, content = raw.split(b"\0", 1)
+        assert head.startswith(b"tree")
+        _, size = head.split(b" ")
+        assert len(content) == int(size)
+        objects = []
+        while True:
+            pos = content.find(b"\x00")
+            if pos == -1:
+                break
+            objects.append(
+                list(
+                    itertools.chain(
+                        *[x.split(b" ") for x in content[: pos + 21].split(b"\x00")]
+                    )
+                )
+            )
+            content = content[pos + 21 :]
+        for o in objects:
+            if name_only:
+                print(o[1].decode("utf-8"))
 
 
 if __name__ == "__main__":
